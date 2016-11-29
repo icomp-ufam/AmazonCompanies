bf02422be7001cae6bc68abb75a41db833932d07
GiseleCristina <gcmn@icomp.ufam.edu.br>
2016-11-28 20:32:26 -0400

    teste

--- views/site/about.php
+++ views/site/about.php
@@ -9,7 +9,7 @@ $this->title = 'Sobre';
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <h2> teste </h1>
     <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
     </p>
