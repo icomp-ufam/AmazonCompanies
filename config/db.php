3ad7c61928150b5490b2c1c40cf35190b2af2950
Pedro Frota <pvmf@icomp.ufam.edu.br>
2017-02-21 01:18:28 -0400

    Removendo arquivo de configuração /config/db.php
    Removendo o arquivo de configuração do banco do repositório

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=amazonCompanies',
-    'username' => 'ac',
-    'password' => '123',
-    'charset' => 'utf8',
-];
