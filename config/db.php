5f134a9a17d0105ac332822f47cf6246c50b8240
Luiz Gustavo <lgpa@icomp.ufam.edu.br>
2017-02-22 11:37:58 -0400

    Re-inserindo o banco de dados

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=amazonCompanies',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
